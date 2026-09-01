fbafd4d028bebecde8df5381b918a8087a735fe6
daniel-leone <123667871+dagron27@users.noreply.github.com>
2026-09-01 10:40:48 -0500

    deploy: update web build (debug export, 2026-09-01T15:40:48Z)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788276695|11060631';
+const CACHE_VERSION = '1788277246|10675358';
 /** @type {string} */
 const CACHE_PREFIX = 'Reel-Deal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
